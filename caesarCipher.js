5fca1f032490cb78ebb5ad0f37f7548d6332bd94
wukongO-O <l.shenglan@gmail.com>
2022-12-14 21:29:15 -0500

    add more unit tests and update caesarCipher function to pass them

--- caesarCipher.js
+++ caesarCipher.js
@@ -1,12 +1,22 @@
 function caesarCipher(str, key) {
     const regex = /[a-zA-Z]/;
+    key = key % 26; 
 
     const encrypt = () => {
         let encryptedStr = '';
 
         for (let i = 0; i < str.length; i++) {
             if (str.charAt(i).match(regex)) {
-                const shiftedCharUnicode = str.charCodeAt(i) + key;
+                if (key === 0) return str;
+                //unicode: 65-90: A-Z; 97-122: a-z
+                let shiftedCharUnicode = str.toLowerCase().charCodeAt(i) + key;
+                //for shifting across z and a - forward & backward
+                if (shiftedCharUnicode > 122) {
+                    shiftedCharUnicode = shiftedCharUnicode - 26;
+                } else if (shiftedCharUnicode < 97) {
+                    shiftedCharUnicode = shiftedCharUnicode + 26;
+                };
+
                 encryptedStr += String.fromCharCode(shiftedCharUnicode);
             } else {
                 encryptedStr += str.charAt(i);
@@ -18,10 +28,19 @@ function caesarCipher(str, key) {
 
     const decrypt = () => {
         let decryptedStr = '';
-        
+
         for (let j = 0; j < str.length; j++) {
             if (str.charAt(j).match(regex)) {
-                const origCharUnicode = str.charCodeAt(j) - key;
+                if (key === 0) return str;
+                
+                let origCharUnicode = str.toLowerCase().charCodeAt(j) - key;
+
+                if (origCharUnicode > 122) {
+                    origCharUnicode = origCharUnicode - 26;
+                } else if (origCharUnicode < 97) {
+                    origCharUnicode = origCharUnicode + 26;
+                };
+                
                 decryptedStr += String.fromCharCode(origCharUnicode);
             } else {
                 decryptedStr += str.charAt(j);
--- caesarCipher.test.js
+++ caesarCipher.test.js
@@ -3,9 +3,41 @@
 const caesarCipher = require('./caesarCipher');
 
 test('shift letter to encrypt', () => {
-    expect(caesarCipher('banana', 2).encrypt()).toBe('dcpcpc');
+    expect(caesarCipher('BananA', 2).encrypt()).toBe('dcpcpc');
+});
+
+test('shift letter across z and a to encrypt', () => {
+    expect(caesarCipher('yes', 3).encrypt()).toBe('bhv');
+});
+
+test('rewind the shift order when key > 26', () => {
+    expect(caesarCipher('pizza', 53).encrypt()).toBe('qjaab');
+});
+
+test('shift letter in backward order when key < 0', () => {
+    expect(caesarCipher('cheese', -2).encrypt()).toBe('afccqc');
+});
+
+test('encrypt to the same str when key = 0', () => {
+    expect(caesarCipher('CAKE', 0).encrypt()).toBe('CAKE');
 });
 
 test('shift letter to decrypt', () => {
-    expect(caesarCipher('mkyk', 2).decrypt()).toBe('kiwi');
+    expect(caesarCipher('MkYk', 2).decrypt()).toBe('kiwi');
+});
+
+test('shift letter from a backward to z to decrypt', () => {
+    expect(caesarCipher('apple', 2).decrypt()).toBe('ynnjc');
+});
+
+test('decrypt to the same str when key = 0', () => {
+    expect(caesarCipher('Watermelon', 0).decrypt()).toBe('Watermelon');
+})
+
+test('decrypt when key > 26', () => {
+    expect(caesarCipher('papaya', 28).decrypt()).toBe('nynywy');
+})
+
+test('decrypt when key < 0', () => {
+    expect(caesarCipher('coconut', -1 ).decrypt()).toBe('dpdpovu');
 })
