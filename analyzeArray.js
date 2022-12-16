439eb0f4f8f1c56766f7915f2c1fc3b31563dfd4
wukongO-O <l.shenglan@gmail.com>
2022-12-15 22:13:20 -0500

    add tests for error cases

--- analyzeArray.js
+++ analyzeArray.js
@@ -1,35 +1,41 @@
 function analyzeArray(arr) {
-
-    const avgVal = () => {
-       if (arr) {
-        const sum = arr.reduce((a, b) => a + b);
-        const avg = sum / arr.length;
-        return Number(Math.round(avg + 'e2') + 'e-2');
-       };
+    const validArr = (arr) => {
+        if (arr.length != 0 && arr.every(item => (typeof item === 'number'))) return true;
+        return false;
     };
 
-    arr.sort((a, b) => a - b);
-
-    const minVal = () => {
-        return arr[0];
+    if (validArr(arr) === true) {
+        const avgVal = () => {
+            const sum = arr.reduce((a, b) => a + b);
+            const avg = sum / arr.length;
+            return Number(Math.round(avg + 'e2') + 'e-2');
+        };
+    
+        arr.sort((a, b) => a - b);
+    
+        const minVal = () => {
+            return arr[0];
+        };
+    
+        const maxVal = () => {
+            return arr[arr.length-1];
+        };
+    
+        const arrLen = () => {
+            return arr.length; 
+        };
+    
+        const obj = {
+            average: avgVal(),
+            min: minVal(),
+            max: maxVal(),
+            length: arrLen()
+        };
+    
+        return obj;
+    } else {
+        return 'Array elements need to be numbers only.'
     };
-
-    const maxVal = () => {
-        return arr[arr.length-1];
-    };
-
-    const arrLen = () => {
-        return arr.length; 
-    };
-
-    const obj = {
-        average: avgVal(),
-        min: minVal(),
-        max: maxVal(),
-        length: arrLen()
-    };
-
-    return obj;
 }
 
 module.exports = analyzeArray;
--- analyzeArray.test.js
+++ analyzeArray.test.js
@@ -4,4 +4,14 @@ const analyzeArr = require('./analyzeArray');
 test('analyze an array of numbers', () => {
     const arr1 = [2, 3, 5, -8, 11, 16, 8];
     expect(analyzeArr(arr1)).toEqual({average: 5.29, min: -8, max: 16, length: 7});
-})
+});
+
+test('show error msg for an array of non-numbers', () => {
+    const arr2 = ['1', '4', '9', 'zero'];
+    expect(analyzeArr(arr2)).toBe('Array elements need to be numbers only.');
+});
+
+test('show error msg for an empty array', () => {
+    const arr3 = [];
+    expect(analyzeArr(arr3)).toBe('Array elements need to be numbers only.');
+});
